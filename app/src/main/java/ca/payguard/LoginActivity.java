57e799480d56df249096e88771c8fd7a3ff76a3e
AlexLavoie <lavoiealex@hotmail.com>
2020-04-21 16:44:50 -0700

    Finished basic version of LoginActivity

--- app/src/main/java/ca/payguard/LoginActivity.java
+++ app/src/main/java/ca/payguard/LoginActivity.java
@@ -1,16 +1,71 @@
 package ca.payguard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.payguard.R;
 
+/**
+ * LoginActivity ensures user is authenticated before
+ * opening MainActivity.
+ */
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Open MainActivity if user is already logged in.
+        if (mAuth.getCurrentUser() != null) {
+            openMainActivity();
+        }
+    }
+
+    /** Logs in user through firebase and opens MainActivity on success. */
+    public void login(View view){
+        EditText username = findViewById(R.id.et_username);
+        EditText password = findViewById(R.id.et_password);
+
+        //Sign in through Firebase Auth
+        mAuth.signInWithEmailAndPassword(username.getText().toString(),
+                password.getText().toString())
+        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    openMainActivity();
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /** Starts MainActivity */
+    private void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
