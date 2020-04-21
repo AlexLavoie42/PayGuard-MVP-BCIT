71f6cd14cd3b5afe11b8eecb67d6296f260886d9
Garrett Tailby <lastgameforlife@hotmail.com>
2020-04-21 14:26:59 -0700

    Added class for payment activity. Switched PlainText Elements into TextView Elements.

---
+++ app/src/main/java/ca/payguard/Payment.java
@@ -0,0 +1,15 @@
+package ca.payguard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.payguard.R;
+
+public class Payment extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
