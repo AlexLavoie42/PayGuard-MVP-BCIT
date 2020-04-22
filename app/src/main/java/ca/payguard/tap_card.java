d96d667af733d8a81067654d182a3bc237edd017
Garrett Tailby <lastgameforlife@hotmail.com>
2020-04-21 17:17:52 -0700

    Added activities for pre-auth and pin-pad. Errors are in connecting activity to java class.

---
+++ app/src/main/java/ca/payguard/tap_card.java
@@ -0,0 +1,14 @@
+package ca.payguard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.payguard.R;
+
+public class tap_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tap_card);
+    }
+}
