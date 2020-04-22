e4ba8ecd1fc1f8f1a9b4258480da9c621ddc9276
Michael Sinclair <michaelkevin.sinclair@gmail.com>
2020-04-22 10:53:16 -0700

    Fixed table layout

--- app/src/main/java/ca/payguard/TableSet.java
+++ app/src/main/java/ca/payguard/TableSet.java
@@ -30,32 +30,34 @@ class TableSet extends ArrayList<Table> {
         int segmentX = width / 20, segmentY = height / 20;
         int size = Math.max(segmentX, segmentY);
         sizes[0] = size;
-        sizes[1] = size * 2;
-        sizes[2] = size * 3;
+        sizes[1] = size * 4;
+        sizes[2] = size * 8;
 
         for(int i = 0; i < 9; i++){
             Table t = new Table();
+            t.setLabel("" + (i+1));
             t.setDimensions(sizes[0], sizes[0]);
             add(t);
         }
 
         Table t = get(0);
-        t.setCoords(13 * segmentX, 2 * segmentY);
+        t.setCoords(2 * segmentX, 6 * segmentY);
         t = get(1);
-        t.setCoords(7 * segmentX, 2 * segmentY);
+        t.setCoords(6 * segmentX, 6 * segmentY);
         t = get(2);
-        t.setCoords(10 * segmentX, 6 * segmentY);
+        t.setCoords(10 * segmentX, 8 * segmentY);
         t = get(3);
-        t.setCoords(3 * segmentX, 10 * segmentY);
+        t.setCoords(2 * segmentX, 10 * segmentY);
         t = get(4);
-        t.setCoords(7 * segmentX, 10 * segmentY);
+        t.setCoords(6 * segmentX, 10 * segmentY);
         t = get(5);
-        t.setCoords(8 * segmentX, 14 * segmentY);
+        t.setCoords(10 * segmentX, 12 * segmentY);
         t = get(6);
-        t.setCoords(3 * segmentX, 18 * segmentY);
+        t.setCoords(2 * segmentX, 14 * segmentY);
         t = get(7);
-        t.setCoords(5 * segmentX, 18 * segmentY);
+        t.setCoords(6 * segmentX, 14 * segmentY);
         t = get(8);
-        t.setCoords(13 * segmentX, 2 * segmentY);
+        t.setCoords(16 * segmentX, 10 * segmentY);
+        t.setDimensions(t.getWidth(), sizes[2]);
     }
 }
