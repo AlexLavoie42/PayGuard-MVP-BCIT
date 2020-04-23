d554155df667d76a21cb9d4c13593216e28b6c9c
Michael Sinclair <michaelkevin.sinclair@gmail.com>
2020-04-23 12:19:58 -0700

    Removed unnecessary imports

--- app/src/main/java/ca/payguard/MainActivity.java
+++ app/src/main/java/ca/payguard/MainActivity.java
@@ -3,18 +3,12 @@ package ca.payguard;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Display;
 import android.view.View;
 import android.widget.Button;
-
 import java.util.ArrayList;
 
 /**
