086b93d98bf9830206fab5ec50dcd32e5758659d
Garrett Tailby <lastgameforlife@hotmail.com>
2020-04-29 14:40:37 -0700

    CanadaPreAuth Setting methods completed, Execute method is mostly complete (Waiting on AuthToken).

--- app/src/main/java/ca/payguard/util/TransactionHandler.java
+++ app/src/main/java/ca/payguard/util/TransactionHandler.java
@@ -1,16 +1,16 @@
 package ca.payguard.util;
 
 public interface TransactionHandler {
-    public boolean setStoreId(String id);
+    public void setStoreId(String id);
 
-    public boolean setDate(String date);
+    public void setDate(String date);
 
-    public boolean setOrderId(String id);
+    public void setOrderId(String id);
 
     /** The numbers on the front of the card. */
-    public boolean setPan(String pan);
+    public void setPan(String pan);
 
-    public boolean setExpDate(String date);
+    public void setExpDate(String date);
 
     /** Executes the initial transaction. Takes the pre-auth $ amount and returns an AuthToken. */
     public AuthToken executeTransaction(String dollars);
