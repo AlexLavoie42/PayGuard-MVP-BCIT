499a16213a81f2b716eb612ba8a268995309888d
Garrett Tailby <lastgameforlife@hotmail.com>
2020-05-06 14:05:23 -0700

    Added more custom exceptions, Started on ControllerService.

---
+++ app/src/main/java/ca/payguard/util/ItemAlreadyThere.java
@@ -0,0 +1,15 @@
+package ca.payguard.util;
+
+public class ItemAlreadyThere extends Exception {
+    public ItemAlreadyThere(String errorMessage, Throwable err){
+        super(errorMessage, err);
+    }
+
+    public ItemAlreadyThere(Throwable err){
+        super("An Item is already there.", err);
+    }
+
+    public ItemAlreadyThere(){
+        super("An Item is already there.");
+    }
+}
---
+++ app/src/main/java/ca/payguard/util/PreAuthFailure.java
@@ -0,0 +1,15 @@
+package ca.payguard.util;
+
+public class PreAuthFailure extends Exception{
+    public PreAuthFailure(String errorMessage, Throwable err){
+        super(errorMessage, err);
+    }
+
+    public PreAuthFailure(Throwable err){
+        super("Error in Pre-Authorization.", err);
+    }
+
+    public PreAuthFailure(){
+        super("Error in Pre-Authorization.");
+    }
+}
