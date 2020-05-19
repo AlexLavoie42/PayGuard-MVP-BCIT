bfb4c9102564b905fba550480ad379aa4fac5efc
Zachary Oyama <oyama905@gmail.com>
2020-05-19 11:18:27 -0700

    Vonage SMS API Class pushed Test message method complete

---
+++ app/src/main/java/ca/payguard/SMSTextMagic.java
@@ -0,0 +1,61 @@
+package ca.payguard;
+
+
+import com.textmagic.sdk.RestClient;
+import com.textmagic.sdk.RestException;
+import com.textmagic.sdk.resource.instance.TMNewMessage;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Collections;
+
+// TextMagic Trial Account. Expires June 4, 2020
+
+public class SMSTextMagic {
+
+    private static final String USERNAME = "";
+    private static final String API_KEY = "";
+    private static final String TEST_NUM = "";
+
+    private static RestClient client;
+
+    private static DecimalFormat formatter = new DecimalFormat("#.##");
+
+
+    public SMSTextMagic() {
+        client = new RestClient(USERNAME, API_KEY);
+    }
+
+    // Send initial SMS showing confirmed pre-auth to
+    // Assume phone number is pre-formatted in E.164 format - Eg. 16041234567
+    public void sendAuthConfirmedSMS(String toNum, double authLimit, String restaurantName) {
+
+        TMNewMessage message = client.getResource(TMNewMessage.class);
+        message.setText("Thank you for using PayGuard at " + restaurantName
+                + ". Your pre-authorization of $" + formatter.format(authLimit) + " has been received. " +
+                "Please reply DONE to receive your bill.");
+        message.setPhones(Arrays.asList(new String[] {toNum}));
+        try {
+            message.send();
+        }
+        catch (final RestException e) {
+            System.out.println(e.getErrors());
+        }
+        System.out.println(message.getId());
+
+    }
+
+    // Test method to ping registered number
+    public void sendTestSMS(String body) {
+        TMNewMessage message = client.getResource(TMNewMessage.class);
+        message.setText(body);
+        message.setPhones(Collections.singletonList(TEST_NUM));
+        try {
+            message.send();
+        }
+        catch (final RestException e) {
+            System.out.println(e.getErrors());
+        }
+        System.out.println(message.getId());
+    }
+}
